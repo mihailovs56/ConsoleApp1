af0af6dd021d2b56214a1139d5a6f42066c4bb77
Михайлов <@DESKTOP-84VD5MI>
2023-10-12 20:15:18 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class programm
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку");
+        string a = Console.ReadLine();
+        string r = ChangeCase(a);
+        Console.WriteLine(r);
+
+        static string ChangeCase(string a)
+        {
+            char[] f = a.ToCharArray();
+            for (int i = 0; i < f.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    f[i] = char.ToUpper(f[i]);
+                }
+                else
+                {
+                    f[i] = char.ToLower(f[i]);
+                }
+            }
+            return new string(f);
+        }
+    }
+}
